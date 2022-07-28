b6913f396672cdb889c7d310ac4683d5d649f6e0
Nicolas Nieloux <nicolas.nieloux@Digital-Grenoble.local>
2022-07-28 14:05:23 +0200

    ajout header

--- header.php
+++ header.php
@@ -1,1 +1,26 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Titre du document</title>
+    <style>
+        li {
+            display: inline-block;
+            margin-right: 10px;
+            color: #778899;
+        }
+    </style>
+</head>
+<body>
+<header>
+    <nav>
+        <ul style="padding:0;">
+            <li>Accueil</li>
+            <li>À propos</li>
+        </ul>
+    </nav>
+    <h1>Bienvenue sur notre page</h1>
+    <hr>
+</header>
+<article>
+    <header>
+
