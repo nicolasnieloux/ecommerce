e065bb3c01e59cf04ee7ce0657abc390f7196a4e
Nicolas Nieloux <nicolas.nieloux@Digital-Grenoble.local>
2022-08-03 16:59:59 +0200

    page php catalogue

---
+++ products.php
@@ -0,0 +1,26 @@
+<?php
+$products = [
+    "iphone" => [
+        "name" => "iPhone",
+        "price" => 45000,
+        "weight" => 200,
+        "discount" => 15,
+        "picture_url" => "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-12-family-select-2021?wid=940&hei=1112&fmt=jpeg&qlt=90&.v=1617135051000",
+    ],
+
+    "ipad" => [
+        "name" => "iPad",
+        "price" => 30000,
+        "weight" => 400,
+        "discount" => 25,
+        "picture_url" => "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/ipad-air-select-wifi-blue-202203?wid=1765&hei=2000&fmt=jpeg&qlt=95&.v=1645065732688",
+    ],
+    "imac" => [
+        "name" => "iMac",
+        "price" => 70000,
+        "weight" => 1200,
+        "discount" => 0,
+        "picture_url" => "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/refurb-2019-imac-215-gallery?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1559961087778",
+    ]
+];
+?>
