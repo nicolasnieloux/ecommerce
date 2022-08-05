ad244420c7ca160536192a1d3334940b8f912e58
Nicolas Nieloux <nicolas.nieloux@Digital-Grenoble.local>
2022-08-05 14:54:46 +0200

    affichage panier avec transporteur

--- cart.php
+++ cart.php
@@ -1,34 +1,41 @@
 <?php require "my_functions.php";
 require "products.php";
 global $products;
-$q = $_POST["quantity"];
+$productQuantity = $_POST["quantity"];
 $key = $_POST["key"];
 $prixTTC = $products[$key]["price"];
 $prixHT = (priceExcludingVAT($products[$key]["price"]));
 $prixHTDiscount = priceExcludingVAT(discountedPrice($products[$key]["price"], $products[$key]["discount"]));
 $TVA = ($prixTTC - $prixHT);
 $TVAdiscount = discountedPrice($products[$key]["price"], $products[$key]["discount"]) - $prixHTDiscount;
-$prixTotal = $prixTTC * $q;
-$transporteur="La Poste";
+$prixTotal = $prixTTC * $productQuantity;
+
+$transporteur = "La Poste";
 if (isset($_POST['transporteur'])) {
     $transporteur = $_POST['transporteur'];
 }
+
+if (isset($_POST['quantity'])) {
+    $productQuantity = $_POST['quantity'];
+}
+
 var_dump($_POST);
 ?>
 
-<?php //ValidQuantity($q); ?>
+<?php validQuantity($productQuantity); ?>
 
 
 <h3><?= $products[$key]["name"]; ?></h3>
-<p>Prix TTC Unitaire :<?php echo FormatPrice($prixTTC); ?></p>
+<p>Prix TTC Unitaire :<?php formatPrice($prixTTC); ?></p>
 
 <?php if ($products[$key]["discount"] != null) { ?>
     <p>Prix Discount TTC Unitaire <?php echo $products[$key]["discount"] . "%" ?>
-        : <?php FormatPrice(discountedPrice($products[$key]["price"], $products[$key]["discount"])); ?></p>
-    <p>Quantité : <?php echo $q; ?></p>
-    <p> Prix Total: <?php FormatPrice(discountedPrice($products[$key]["price"], $products[$key]["discount"]) * $q); ?> </p>
-    <p>Prix Total HT :<?php FormatPrice($prixHTDiscount * $q); ?></p>
-    <p>TVA: <?php FormatPrice($TVAdiscount * $q); ?></p>
+        : <?php formatPrice(discountedPrice($products[$key]["price"], $products[$key]["discount"])); ?></p>
+    <p>Quantité : <?php echo $productQuantity; ?></p>
+    <p> Prix
+        Total: <?php formatPrice(discountedPrice($products[$key]["price"], $products[$key]["discount"]) * $productQuantity); ?> </p>
+    <p>Prix Total HT :<?php formatPrice($prixHTDiscount * $productQuantity); ?></p>
+    <p>TVA: <?php formatPrice($TVAdiscount * $productQuantity); ?></p>
 
 
     <form method=post>
@@ -42,17 +49,18 @@ var_dump($_POST);
         <input type="hidden" value="<?= $_POST["key"] ?>" name="key">
         <input type="submit" value="Valider">
     </form>
-    <p>Prix transport : <?php FormatPrice(deliver($transporteur, $products[$key]["weight"] ,$prixTotal));?></p>
+    <p>Prix transport : <?php formatPrice(deliver($transporteur, $products[$key]["weight"], $prixTotal)); ?></p>
 
 
-    <p> Prix Total TTC: <?php echo FormatPrice(discountedPrice($products[$key]["price"], $products[$key]["discount"]) * $q + deliver($transporteur, $products[$key]["weight"] ,$prixTotal)) ; ?> </p>
+    <p> Prix Total
+        TTC: <?php formatPrice(discountedPrice($products[$key]["price"], $products[$key]["discount"]) * $productQuantity + deliver($transporteur, $products[$key]["weight"], $prixTotal)); ?> </p>
 <?php } else { ?>
 
 
-    <p>Quantité : <?php echo $q; ?></p>
-    <p> Prix Total: <?php FormatPrice($prixTotal); ?> </p>
-    <p>Prix Total HT :<?php  FormatPrice($prixHT * $q); ?></p>
-    <p>TVA: <?php FormatPrice($TVA * $q); ?></p>
+    <p>Quantité : <?php echo $productQuantity; ?></p>
+    <p> Prix Total: <?php formatPrice($prixTotal); ?> </p>
+    <p>Prix Total HT :<?php formatPrice($prixHT * $productQuantity); ?></p>
+    <p>TVA: <?php formatPrice($TVA * $productQuantity); ?></p>
 
     <form method=post>
         <label for="transporteur">Choisir son transporteur:</label>
@@ -64,10 +72,15 @@ var_dump($_POST);
         <input type="hidden" value="<?= $_POST["quantity"]; ?>" name="quantity">
         <input type="hidden" value="<?= $_POST["key"] ?>" name="key">
         <input type="submit" value="Valider">
-    </form>
-    <p>Prix transport : <?php FormatPrice(deliver($transporteur, $products[$key]["weight"] ,$prixTotal));?></p>
 
-     <p> Prix Total TTC: <?php FormatPrice($products[$key]["price"] * $q + deliver($transporteur, $products[$key]["weight"] ,$prixTotal)) ; ?> </p>
+    </form>
+    <p>Prix transport
+        : <?php formatPrice(deliver($transporteur, $products[$key]["weight"] * $productQuantity, $prixTotal)); ?></p>
+
+    <p> Prix Total
+        TTC: <?php formatPrice($products[$key]["price"] * $productQuantity + deliver($transporteur, $products[$key]["weight"] * $productQuantity, $prixTotal)); ?> </p>
+
+
 <?php }
 ?>
 <img src="<?php echo $products[$key]["picture_url"]; ?> " alt="Photo d'un iphone" width="250px" height="250px">
